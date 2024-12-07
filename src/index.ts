cd87400abbcd0b45f632f0c3d5fe7876e6eaa502
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-07 12:32:30 -0800

    Takng out the debug code after testing

--- src/index.ts
+++ src/index.ts
@@ -30,16 +30,6 @@ vehicles.push(car1);
 vehicles.push(truck1);
 vehicles.push(motorbike1);
 
-/* 
- * for debugging
-const car2 = new Car(Cli.generateVin(), 'red', 'Chevrolet', 'Corvette', 1978, 3444, 123, []);
-const truck2 = new Truck(Cli.generateVin(), 'yellow', 'Chevrolet', '3100', 1946, 1452, 90, [], 2300);
-const motorbike2 = new Motorbike(Cli.generateVin(), 'orange', 'Triumph', 'TR6', 1961, 1130, 120, motorbike1Wheels);
-vehicles.push(car2);
-vehicles.push(truck2);
-vehicles.push(motorbike2);
- */
-
 // create a new instance of the Cli class
 const cli = new Cli(vehicles);
 
